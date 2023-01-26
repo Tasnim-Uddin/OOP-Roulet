82a93f06930f39ef88e91aa8fc679ef8bb0e7b81
Tasnim Uddin Syed <112864578+Tasnim-Uddin@users.noreply.github.com>
2023-01-26 14:50:27 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -64,7 +64,88 @@ class Wheel:
         return self.bins[bin]
 
 
-w = Wheel()
-print(w.next())
-print(w.next())
-print(w.next())
+class BinBuilder:
+    """
+    Creates all of the winning Outcomes and adds them to each bin on the wheel
+    Each bin has 12-14 different ways that it can be a winner e.g. 1 can be straight, street, corner...
+    Chapter 8, pages 53-58
+    """
+
+    def __init__(self):
+        pass
+
+    def buildBins(self, wheel):
+        pass
+
+    def straightBets(self):
+        """
+        Bet on a single number paying at 35:1
+        38 bets / 38 outcomes
+        """
+        outcomes = []
+        for n in range(0, 38):
+            if n == 37:
+                n_outcome = Outcome(name='00', odds=35)
+                outcomes.append((n, n_outcome))
+            else:
+                n_outcome = Outcome(name=str(n), odds=35)
+                outcomes.append((n, n_outcome))
+        return outcomes
+
+    def splitBets(self):
+        """
+        Adjacent pair of numbers (column or row) paying at 17:1
+        114 bets / 114 outcomes
+        """
+        outcomes = []
+        return outcomes
+
+    def streetBets(self):
+        """
+        3 numbers in a single row paying at 11:1
+        12 possible bets / 36 outcomes
+        """
+        outcomes = []
+        return outcomes
+
+    def lineBets(self):
+        """
+        A 6 number block (2 street bets) paying at 5:1
+        11 possible bets
+        """
+        outcomes = []
+        for r in range(0, 11):
+            n = 3*r + 1
+            line = f"{n}-{n+1}-{n+2}-{n+3}-{n+4}-{n+5}"
+            n_outcome = Outcome(name=str(f"Line {line}"), odds=5)
+            outcomes.append((r, n_outcome))
+        return outcomes
+
+    def dozenBets(self):
+        """
+        Each number is member of one of three dozens paying at 2:1
+        3 possible bets
+        """
+        outcomes = []
+        return outcomes
+
+    def cornerBets(self):
+        """
+        A square of 4 numbers paying at 8:1
+        22 possible bets / 88 outcomes
+        """
+        outcomes = []
+        return outcomes
+
+    def outsideBets(self):
+        """
+        All other bets e.g. Red/Black, Low/High, Even/Odd
+        """
+        outcomes = []
+        return outcomes
+
+
+bb = BinBuilder()
+print(bb.straightBets())
+print(len(bb.straightBets()))
+print(bb.lineBets())
